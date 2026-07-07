dfd0b369458d1ac6584ec1f9435a5c9e4d77d4ec
SavanSavaliya07 <saavansavaliya702@gmail.com>
2026-07-07 15:06:35 +0530

    Time: 40 ms (73.62%), Space: 53.6 MB (47.31%) - LeetHub

---
+++ 2703-return-length-of-arguments-passed/2703-return-length-of-arguments-passed.js
@@ -0,0 +1,14 @@
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+let i = 0;
+while (i < args.length) {
+  i++;
+}return i
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
