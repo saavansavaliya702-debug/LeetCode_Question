5efce47ed8b76bae9db06e7a693aefb076470359
SavanSavaliya07 <saavansavaliya702@gmail.com>
2026-07-09 09:19:25 +0530

    Time: 1 ms (92.97%), Space: 55.7 MB (73.19%) - LeetHub

--- 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.js
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.js
@@ -1,9 +1,9 @@
 var numberOfSpecialChars = function (word) {
   let count = 0;
 
-  for (let i = 65; i <= 90; i++) {       // Loop A–Z (uppercase range)
+  for (let i = 65; i <= 90; i++) {       
     const upper = String.fromCharCode(i);
-    const lower = String.fromCharCode(i + 32);  // a–z is +32 from A–Z
+    const lower = String.fromCharCode(i + 32);  
 
     if (word.includes(upper) && word.includes(lower)) {
       count++;
