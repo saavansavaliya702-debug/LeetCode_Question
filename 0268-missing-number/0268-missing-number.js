a0211e9aad43c2833ca0eab6c9f19702ffaa284f
SavanSavaliya07 <saavansavaliya702@gmail.com>
2026-07-22 20:17:03 +0530

    Time: 0 ms (100%), Space: 56.6 MB (52.42%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+ const n = nums.length;
+
+
+    const expectedSum = (n * (n + 1)) / 2;
+
+    let actualSum = 0;
+    for (let i = 0; i < n; i++) {
+        actualSum += nums[i];
+    }
+
+    return expectedSum - actualSum;    
+};
